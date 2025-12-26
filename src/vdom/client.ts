32cf77c2b4567489a986ceaae01c4d1a8d7523e9
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2025-12-26 13:10:43 +0800

    Revert "Update client.ts"
    This reverts commit faae2f91917a9785f33c6560207f8964673e3da9.

--- src/vdom/client.ts
+++ src/vdom/client.ts
@@ -26,14 +26,19 @@ function _toFrag(vi: VirtualItem): DocumentFragment {
     if (vi["__postactItem"] == "virtual-text-node") {
       // VirtualTextNode
       const vtn = vi as VirtualTextNode;
-      let tn = window.document.createTextNode(vtn.data);
+      const tn = window.document.createTextNode(vtn.data);
 
       if (vtn.subscribable)
         vtn.subscribable.subscribe((value) => {
-          const frag = _toFrag(transformArgToVirtualItem(value));
-            )
-          tn.parentElement?.replaceChild(frag, tn);
-          tn = frag
+          if (isPrimitive(value)) {
+            // then we'll keep it simple
+            tn.textContent = value.toString();
+          } else if (value === null) {
+            tn.textContent = "";
+          } else {
+            const frag = _toFrag(transformArgToVirtualItem(value));
+            tn.parentElement?.replaceChild(frag, tn);
+          }
         });
 
       fragment.appendChild(tn);
